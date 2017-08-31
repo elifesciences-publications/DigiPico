7e3fcb761cb5c765efbd485bc9bc33326dfd34e1
Sahand <sharifza@in.tum.de>
2017-08-31 22:44:41 +0200

    Generator seems to be fine. Large Set give 95% accuracy, have to add back the FP,...

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.utils import shuffle
 import preprocess
 import linecache
+import keras
 
 if __name__ == "__main__":
     weights_path = ''
-    # weights_path = 'mutation_logistic_wts.h5'
+    # weights_path = 'Important_Weights/mutation_logistic_wts.h5'
 
     # fix random seed for reproducibility
     seed = 7
@@ -23,27 +24,29 @@ if __name__ == "__main__":
     over_sampling_rate = 1  # ATTENTION: MAX 8 in current set
 
     # Set tensorboard callback
-    #tbCallBack = keras.callbacks.TensorBoard(log_dir='./summary/log3')
+    tbCallBack = keras.callbacks.TensorBoard(log_dir='./summary/log3')
 
     # load dataset
     # x_train, y_train, x_test, y_test = preprocess.prep_data('Data/Sahand_Chr22_No-Filter.csv','Data/Sahand_Chr21_Filter.csv', over_sampling_rate)
     # train_data, test_data = preprocess.prep_data_all('Data/Filter.csv', over_sampling_rate, cols=range(1, 33))
     # train, test = preprocess.load_preprocessed_data('Small_PD/')
 
-    folder = 'Small_PD/'
+    test_folder = 'Large_PD/'
+    train_folder = 'Large_PD/'
+    # wc -l filename to extract number of rows of csv file
+    train_size = 18240
+    test_size = 106946
+    # train_size = 2800200
+    # test_size = 6268829
 
-    with open(folder + 'train.csv', 'r') as data_file:
+    with open(train_folder + 'train.csv', 'r') as data_file:
         for i, line_x in enumerate(data_file):
             if i == 0:
                 line_x = line_x.rstrip().split(',')
                 break
 
     input_dim = len(line_x) - 1
-    # wc -l filename to extract number of rows of csv file
-    train_size = 18240
-    test_size = 106946
-    # train_size = 2800200
-    # test_size = 6268829
+
     train_steps_per_epoch = int(train_size/batch_size)
     test_steps_per_epoch = int(test_size/batch_size)
     # input_dim = x_train.shape[1]
@@ -74,13 +77,13 @@ if __name__ == "__main__":
     #                     verbose=1,
     #                     )#validation_data=(x_test, y_test))  # , callbacks=[tbCallBack])
     if weights_path == '':
-        model.fit_generator(preprocess.generate_data_from_file(folder + 'train.csv', feature_size=input_dim, batch_size=batch_size),
-                            steps_per_epoch=train_steps_per_epoch, nb_epoch=epochs, verbose=1)
+        model.fit_generator(preprocess.generate_data_from_file(train_folder + 'train.csv', feature_size=input_dim, batch_size=batch_size),
+                            steps_per_epoch=train_steps_per_epoch, nb_epoch=epochs, verbose=1, callbacks=[tbCallBack])
 
-    score = model.evaluate_generator(preprocess.generate_data_from_file(folder + 'test.csv', feature_size=input_dim, batch_size=batch_size), test_steps_per_epoch)
+    score = model.evaluate_generator(preprocess.generate_data_from_file(test_folder + 'test.csv', feature_size=input_dim, batch_size=batch_size), test_steps_per_epoch)
 
     print('Test loss:', score[0])
-    print('Test accuracy:', score[1])
+    print('Test accuracy:', score[1]);
 
     # y_pred = model.predict_generator(preprocess.generate_data_from_file('test.csv', feature_size=input_dim, batch_size=batch_size), steps_per_epoch)
     # # y_pred = model.predict(x_test)
